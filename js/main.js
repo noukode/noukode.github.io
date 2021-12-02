0c28fd3134421ad2efb6408aa1adfe47dd6be648
maulan909 <maulanahilmi909@gmail.com>
2021-12-02 20:50:11 +0700

    update project terbaru di section what i'm working on

--- js/main.js
+++ js/main.js
@@ -1,21 +1,28 @@
 AOS.init();
-function sendMessage(){
+function sendMessage() {
     let chat_id = 832974752;
     let name = document.getElementById('name').value;
     let email = document.getElementById('email').value;
     let message = document.getElementById('message').value;
-    let text = 'Name : ' + name +  "\nEmail : " +  email + "\nMessage:\n"+ message; 
+    let text =
+        'Name : ' + name + '\nEmail : ' + email + '\nMessage:\n' + message;
     var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
+    xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-           // Typical action to be performed when the document is ready:
-           document.location.href = document.URL;
+            // Typical action to be performed when the document is ready:
+            document.location.href = document.URL;
         }
     };
-    xhttp.open("POST", "https://api.telegram.org/bot1831433851:AAHyI_o4WNHY-Ljy5evW_Z-oM88Wvh1lXAQ/sendMessage", true);
+    xhttp.open(
+        'POST',
+        'https://api.telegram.org/bot1831433851:AAHyI_o4WNHY-Ljy5evW_Z-oM88Wvh1lXAQ/sendMessage',
+        true
+    );
     xhttp.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    xhttp.send(JSON.stringify({
-        "chat_id":chat_id,
-        "text" : text
-    }));
+    xhttp.send(
+        JSON.stringify({
+            chat_id: chat_id,
+            text: text,
+        })
+    );
 }
