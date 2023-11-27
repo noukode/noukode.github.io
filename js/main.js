10b530df1fbb3838766f6e9cbe389068155c361a
Hilmi Maulana <maulanahilmi909@gmail.com>
2023-11-27 20:52:41 +0700

    update: Hero, Skills, Experience, Project

--- js/main.js
+++ js/main.js
@@ -27,19 +27,192 @@ function sendMessage() {
     );
 }
 
+let project;
+
 $(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
-        // center: true,
-        items:5,
-        loop:true,
-        margin:10,
-        nav:true,
-        stagePadding:200,
-        autoplay:true,
-        responsive:{
-            600:{
-                items:2
-            }
+    $.ajax({ 
+        type: 'GET', 
+        url: '/js/skill.json',
+        dataType: 'json',
+        success: function (data) { 
+            console.log(data);
+            let html = ``;
+            let i = 100;
+            data.forEach(item => {
+                html += `
+                <div
+                    class="col-6 col-lg-2 mb-3"
+                    data-aos="fade-down"
+                    data-aos-offset="20"
+                    data-aos-delay="${i}"
+                >
+                    <div class="card">
+                        <img
+                            src="img/${item.image}"
+                            class="card-img-top"
+                            alt="${item.name}"
+                        />
+                        <div class="card-body">
+                            <h3 class="text-center">${item.name}</h3>
+                        </div>
+                    </div>
+                </div>
+                `
+                i += 100;
+            })
+            $('#tools').html(html);
         }
     });
+    $.ajax({ 
+        type: 'GET', 
+        url: '/js/learning.json',
+        dataType: 'json',
+        success: function (data) { 
+            console.log(data);
+            let html = ``;
+            let i = 100;
+            data.forEach(item => {
+                html += `
+                <div
+                    class="col-6 col-lg-2 mb-3"
+                    data-aos="fade-down"
+                    data-aos-offset="20"
+                    data-aos-delay="${i}"
+                >
+                    <div class="card">
+                        <img
+                            src="img/${item.image}"
+                            class="card-img-top"
+                            alt="${item.name}"
+                        />
+                        <div class="card-body">
+                            <h3 class="text-center">${item.name}</h3>
+                        </div>
+                    </div>
+                </div>
+                `
+                i += 100;
+            })
+            $('#learning').html(html);
+        }
+    });
+    $.ajax({ 
+        type: 'GET', 
+        url: '/js/education.json',
+        dataType: 'json',
+        success: function (data) { 
+            let html = ``;
+            data.forEach(item => {
+                html += `
+                <li>
+                    <div class="icon">
+                        <i class="text-dark bi bi-mortarboard-fill"></i>
+                    </div>
+                    <span class="time">${item.start} - ${item.end}</span>
+                    <h5>${item.name} - ${item.major}</h5>
+                    <p>${item.detail}</p>
+                </li>
+                `
+            })
+            $('#education').html(html);
+        }
+    });
+    $.ajax({ 
+        type: 'GET', 
+        url: '/js/job.json',
+        dataType: 'json',
+        success: function (data) { 
+            let html = ``;
+            data.forEach(item => {
+                html += `
+                <li>
+                    <div class="icon">
+                        <i class="text-dark bi bi-briefcase-fill"></i>
+                    </div>
+                    <span class="time">${item.start} - ${item.end}</span>
+                    <h5>${item.major} - ${item.name}</h5>
+                    <p>${item.detail}</p>
+                </li>
+                `
+            })
+            $('#job').html(html);
+        }
+    });
+    $.ajax({ 
+        type: 'GET', 
+        url: '/js/project.json',
+        dataType: 'json',
+        success: function (data) { 
+            project = data;
+            let html = ``;
+            data.sort((a, b) => b.id - a.id).forEach(item => {
+                html += `<div
+                class="card card-project bg-dark"
+                data-bs-toggle="modal"
+                data-bs-target="#projectModal"
+                data-id="${item.id}"
+                >
+                    <img
+                        src="img/${item.cover}"
+                        class="card-img"
+                        alt="${item.name}"
+                    />
+                    <div class="card-img-overlay">
+                        <div class="card-text">${item.category}</div>
+                        <div class="card-title h4 mt-4">
+                            ${item.name}
+                        </div>
+                    </div>
+                </div>`
+            })
+            $('#project').html(html);
+            $('.owl-carousel').owlCarousel({
+                // center: true,
+                items:5,
+                loop:true,
+                margin:10,
+                nav:true,
+                stagePadding:0,
+                autoplay:true,
+                responsiveClass:true,
+                responsive:{
+                    0:{
+                        items:1
+                    },
+                    576:{
+                        items:2,
+                    },
+                    768:{
+                        items:2,
+                    },
+                    992:{
+                        items:3,
+                    },
+
+                }
+            });
+        }
+    });
+
+    $('#project').on('click', '.card-project', function(e){
+        console.log($('#projectModalLabel'));
+        let data = project.find(item => item.id === $(this).data('id'));
+        $('#projectModalLabel').html(data.name);
+
+        let body = `
+            <p class="fw-bold">Description :</p>
+                <p>${data.desc}</p>
+                <p class="fw-bold">Type :</p>
+                <p>${data.type}</p>
+                <p class="fw-bold">${data.subTitle} :</p>
+                <ul>
+                    ${data.detailsSubTitle.map(item => `<li>${item}</li>`).join('')}
+                </ul>
+                <p class="fw-bold">Technology :</p>
+                <ul>
+                ${data.techStack.map(item => `<li>${item}</li>`).join('')}
+                </ul>
+        `;
+        $('#projectModalBody').html(body);
+    })
 })
