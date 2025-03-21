73280bdf5e5eea8fc0af990af225597fc6ff8ee4
Hilmi Maulana <maulanahilmi909@gmail.com>
2025-03-21 23:13:04 +0700

    feat: new theme

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    content: ['index.html'],
+    theme: {
+        container: {
+            center: true,
+            padding: '16px',
+        },
+        extend: {
+            colors: {
+                primary: '#01A6FF',
+                dark: '#0f172a',
+                secondary: '#62748e',
+            },
+            screens: {
+                '2xl': '1320px',
+            },
+        },
+    },
+    plugins: [],
+}
